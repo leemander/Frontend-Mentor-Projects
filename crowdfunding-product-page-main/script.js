e8cd90b2099af522ba70dd08fb8ed3ee09f00d4e
crackerFactory64 <lee.mander@gmail.com>
2021-09-03 17:03:41 +0100

    stats update functionality

--- crowdfunding-product-page-main/script.js
+++ crowdfunding-product-page-main/script.js
@@ -10,6 +10,10 @@ const bookmarkButton = document.getElementById("bookmark-button");
 const bambooButton = document.getElementById("bamboo-button");
 const blackButton = document.getElementById("black-button");
 
+const funds = document.getElementById("funds");
+const backers = document.getElementById("backers");
+const progressBar = document.getElementById("progress-bar");
+
 const selectionModal = document.getElementById("selection-modal");
 
 const pledgeRadios = document.getElementsByClassName("pledge__radio");
@@ -22,7 +26,13 @@ const noRewardPledgeDiv = document.getElementById("pledge-div-no-reward");
 const bambooPledgeDiv = document.getElementById("pledge-div-bamboo");
 const blackPledgeDiv = document.getElementById("pledge-div-black");
 
-const continueButtons = document.getElementsByClassName("enter__button");
+const continueButton1 = document.getElementById("continue-button-1");
+const continueButton2 = document.getElementById("continue-button-2");
+const continueButton3 = document.getElementById("continue-button-3");
+
+const pledge1 = document.getElementById("pledge1");
+const pledge2 = document.getElementById("pledge2");
+const pledge3 = document.getElementById("pledge3");
 
 const closeSelectionModalButton = document.getElementById(
   "close-selection-modal"
@@ -82,6 +92,37 @@ const clearPledges = () => {
   }
 };
 
+const updateStats = (pledge) => {
+  let oldFunds = funds.innerHTML.substring(1).split("");
+  oldFunds.splice(oldFunds.indexOf(","), 1);
+  oldFunds = Number(oldFunds.join(""));
+  console.log(oldFunds);
+
+  let newFunds = oldFunds + Number(pledge.value);
+  let newFundsInt = newFunds;
+  newFunds = newFunds.toString().split("");
+  if (newFunds.length == 5) {
+    newFunds.splice(2, 0, ",");
+  } else if (newFunds.length == 6) {
+    newFunds.splice(3, 0, ",");
+  }
+  newFunds.splice(0, 0, "$");
+  let finalFunds = "";
+  for (let i = 0; i < newFunds.length; i++) {
+    finalFunds += newFunds[i];
+  }
+  console.log(finalFunds);
+  funds.innerHTML = finalFunds;
+
+  backers.innerHTML = "5,008";
+
+  if (newFundsInt / 1000 > 100) {
+    progressBar.style.width = "100%";
+  } else {
+    progressBar.style.width = newFundsInt / 1000 + "%";
+  }
+};
+
 /* listeners */
 
 hamburger.addEventListener("click", () => {
@@ -133,12 +174,23 @@ blackRadio.addEventListener("change", () => {
   showPledgeDiv(blackRadio, blackPledgeDiv);
 });
 
-for (let i = 0; i < continueButtons.length; i++) {
-  continueButtons[i].addEventListener("click", () => {
-    showHideModal(selectionModal);
-    showHideModal(successModal);
-  });
-}
+continueButton1.addEventListener("click", () => {
+  updateStats(pledge1);
+  showHideModal(selectionModal);
+  showHideModal(successModal);
+});
+
+continueButton2.addEventListener("click", () => {
+  updateStats(pledge2);
+  showHideModal(selectionModal);
+  showHideModal(successModal);
+});
+
+continueButton3.addEventListener("click", () => {
+  updateStats(pledge3);
+  showHideModal(selectionModal);
+  showHideModal(successModal);
+});
 
 successButton.addEventListener("click", () => {
   clearPledges();
