921c815c755f00254ba913bfb45f828fe9356a54
crackerFactory64 <lee.mander@gmail.com>
2022-02-16 14:01:36 +0000

    desktop styling

--- static-job-listings-master/src/Job.js
+++ static-job-listings-master/src/Job.js
@@ -16,25 +16,31 @@ export default function Job(props) {
 
   return (
     <article className={props.featured ? "job featured" : "job"}>
-      <img className="job__logo" src={props.logo} />
-      <div className="job__company-container">
-        <h2 className="job__company">{props.company}</h2>
-        {props.new && (
-          <span className="job__new">
-            <p>NEW!</p>
-          </span>
-        )}
-        {props.featured && <span className="job__featured">FEATURED</span>}
+      <div className="job__data">
+        <img className="job__logo" src={props.logo} />
+        <div>
+          <div className="job__company-container">
+            <h2 className="job__company">{props.company}</h2>
+            {props.new && (
+              <span className="job__new">
+                <p>NEW!</p>
+              </span>
+            )}
+            {props.featured && <span className="job__featured">FEATURED</span>}
+          </div>
+          <a href="" className="job__position">
+            {props.position}
+          </a>
+          <div className="job__info">
+            <span>{props.postedAt}</span>
+            <span>&#8226;</span>
+            <span>{props.contract}</span>
+            <span>&#8226;</span>
+            <span>{props.location}</span>
+          </div>
+        </div>
       </div>
-      <h3 className="job__position">{props.position}</h3>
-      <div className="job__info">
-        <span>{props.postedAt}</span>
-        <span>&#8226;</span>
-        <span>{props.contract}</span>
-        <span>&#8226;</span>
-        <span>{props.location}</span>
-      </div>
-      <hr></hr>
+      <hr className="mobile-only"></hr>
       <div className="job__keywords">{keywordsEl}</div>
     </article>
   );
