e9b0ffe927b69b46bc7692e9b4f9f9ed8434d17f
crackerFactory64 <lee.mander@gmail.com>
2022-02-18 14:19:33 +0000

    styling

--- rest-countries-api-with-color-theme-switcher-master/src/App.js
+++ rest-countries-api-with-color-theme-switcher-master/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Country from "./components/Country";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   const [countries, setCountries] = React.useState([]);
@@ -29,12 +33,6 @@ export default function App() {
     });
 
     !results.length ? alert("no results") : setFilteredCountries(results);
-
-    /*setFilteredCountries(() => {
-      return countriesEl.filter((country) => {
-        return country.props.name.toLowerCase().includes(term);
-      });
-    });*/
   }
 
   const countriesEl = countries.map((country, index) => {
@@ -54,32 +52,37 @@ export default function App() {
     <div>
       <header className="header">
         <h1>Where in the world?</h1>
-        <button className="header__dark-mode-toggle">Dark mode</button>
+        <button className="header__dark-mode-toggle">
+          <FontAwesomeIcon icon={faMoon} />
+          Dark Mode
+        </button>
       </header>
       <main className="main">
         <div className="container">
           <div className="main__search-container">
-            <span>🔎</span>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
             <input
               className="main__search"
               placeholder="Search for a country..."
-              onKeyUp={(e) => {
+              onChange={(e) => {
                 search(e);
               }}
             />
           </div>
-          <select
-            className="main__filter"
-            defaultValue=""
-            onChange={(e) => filter(e)}
-          >
-            <option value="">Filter by Region</option>
-            <option value="Africa">Africa</option>
-            <option value="Americas">Americas</option>
-            <option value="Asia">Asia</option>
-            <option value="Europe">Europe</option>
-            <option value="Oceania">Oceania</option>
-          </select>
+          <div className="main__filter-container">
+            <select
+              className="main__filter"
+              defaultValue=""
+              onChange={(e) => filter(e)}
+            >
+              <option value="">Filter by Region</option>
+              <option value="Africa">Africa</option>
+              <option value="Americas">Americas</option>
+              <option value="Asia">Asia</option>
+              <option value="Europe">Europe</option>
+              <option value="Oceania">Oceania</option>
+            </select>
+          </div>
           <div className="main__countries">
             {filteredCountries.length ? filteredCountries : countriesEl}
           </div>
