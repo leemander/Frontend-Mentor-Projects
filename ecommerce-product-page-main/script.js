94a73f168a56fcb5adb83832ea671421a34543d9
crackerFactory64 <lee.mander@gmail.com>
2022-03-01 14:04:56 +0000

    empty cart mobile design and functionality

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -1,15 +1,21 @@
 const openMobileMenu = document.getElementById("open-mobile-menu");
 const mobileMenu = document.getElementById("mobile-menu");
 const closeMobileMenu = document.getElementById("close-mobile-menu");
+const cartBtn = document.getElementById("cart-btn");
+const cart = document.getElementById("cart");
 
-function toggleMenu() {
-  mobileMenu.classList.toggle("open");
+function toggleAside(aside) {
+  aside === "menu"
+    ? mobileMenu.classList.toggle("open")
+    : cart.classList.toggle("open");
 }
 
-openMobileMenu.addEventListener("click", toggleMenu);
-closeMobileMenu.addEventListener("click", toggleMenu);
+openMobileMenu.addEventListener("click", () => toggleAside("menu"));
+closeMobileMenu.addEventListener("click", () => toggleAside("menu"));
 mobileMenu.addEventListener("click", (event) => {
   if (event.target === mobileMenu) {
-    toggleMenu();
+    toggleAside("menu");
   }
 });
+
+cartBtn.addEventListener("click", toggleAside);
