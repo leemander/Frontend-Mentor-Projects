f8df54d31a9af4c60493b5fc7995813dd2a8f1bb
crackerFactory64 <lee.mander@gmail.com>
2022-03-01 14:23:21 +0000

    mobile image gallery functionality

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -3,11 +3,33 @@ const mobileMenu = document.getElementById("mobile-menu");
 const closeMobileMenu = document.getElementById("close-mobile-menu");
 const cartBtn = document.getElementById("cart-btn");
 const cart = document.getElementById("cart");
+const currentImg = document.getElementById("gallery");
+const nextImg = document.getElementById("nxt-img-btn");
+const prevImg = document.getElementById("prev-img-btn");
 
 function toggleAside(aside) {
   aside === "menu"
     ? mobileMenu.classList.toggle("open")
     : cart.classList.toggle("open");
+}
+
+let imageIndex = 0;
+
+function changeImage(target) {
+  const images = [
+    "./images/image-product-1.jpg",
+    "./images/image-product-2.jpg",
+    "./images/image-product-3.jpg",
+    "./images/image-product-4.jpg",
+  ];
+
+  if (target === nextImg) {
+    imageIndex < images.length - 1 ? imageIndex++ : (imageIndex = 0);
+  } else if (target === prevImg) {
+    imageIndex > 0 ? imageIndex-- : (imageIndex = 3);
+  }
+
+  currentImg.style.backgroundImage = `url(${images[imageIndex]})`;
 }
 
 openMobileMenu.addEventListener("click", () => toggleAside("menu"));
@@ -17,5 +39,6 @@ mobileMenu.addEventListener("click", (event) => {
     toggleAside("menu");
   }
 });
-
 cartBtn.addEventListener("click", toggleAside);
+nextImg.addEventListener("click", (event) => changeImage(event.target));
+prevImg.addEventListener("click", (event) => changeImage(event.target));
