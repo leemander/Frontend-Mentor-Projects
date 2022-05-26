02a121ecdab3520a8b4bb4eee7404fc402a4c49b
crackerFactory <lee.mander@gmail.com>
2022-05-26 15:05:11 +0100

    polishing

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -1,4 +1,15 @@
 const currentDay = new Date().getDay();
+
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
 const data = [
   {
     day: 1,
@@ -29,44 +40,37 @@ const data = [
     amount: 25.48,
   },
 ];
-const days = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
 
 const barsEl = document.getElementById("bars");
-const totalEl = document.getElementById("total");
 
 let total = data.reduce((accumulator, day) => accumulator + day.amount, 0);
 
-//generates the bars for the bar chart
-data.forEach((day) => {
-  const barContainer = document.createElement("div");
-  barContainer.tabIndex = 0;
-  barContainer.ariaLabel = `${days[day.day - 1]}: $${day.amount}`;
-  barContainer.classList.add("chart__bar-container");
+function generateBars() {
+  data.forEach((day) => {
+    const barContainer = document.createElement("div");
+    barContainer.tabIndex = 0;
+    barContainer.ariaLabel = `${days[day.day - 1]}: $${day.amount}`;
+    barContainer.classList.add("chart__bar-container");
 
-  //figures out how tall to make the current
-  //bar based on what percentage of the total it represents
+    //figures out how tall to make the current
+    //bar based on what percentage of the total it represents
 
-  barContainer.style.height = `${(day.amount / total) * 375}%`;
+    barContainer.style.height = `${(day.amount / total) * 375}%`;
 
-  const bar = document.createElement("div");
-  bar.classList.add("chart__bar");
-  if (currentDay === day.day) {
-    bar.classList.add("chart__bar--current");
-  }
+    const bar = document.createElement("div");
+    bar.classList.add("chart__bar");
+    if (currentDay === day.day) {
+      bar.classList.add("chart__bar--current");
+    }
 
-  const barPopup = document.createElement("div");
-  barPopup.classList.add("chart__popup");
-  barPopup.innerText = `$${day.amount}`;
+    const barPopup = document.createElement("div");
+    barPopup.classList.add("chart__popup");
+    barPopup.innerText = `$${day.amount}`;
 
-  barContainer.append(bar);
-  barContainer.append(barPopup);
-  barsEl.append(barContainer);
-});
+    barContainer.append(bar);
+    barContainer.append(barPopup);
+    barsEl.append(barContainer);
+  });
+}
+
+generateBars();
