6035133a5160268c9d669917b702413200f83f61
crackerFactory <lee.mander@gmail.com>
2022-10-20 15:01:30 +0100

    more functionality

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -15,14 +15,26 @@ function formatNumber(num) {
 function handleButtonPress() {
   if (!operator) {
     if (+this.dataset.button >= 0 || this.dataset.button === ".") {
+      if (result) {
+        result = 0;
+        xRegister = 0;
+      }
       xRegister += this.dataset.button;
-    } else if (operators.includes(this.dataset.button)) {
-      operator = this.dataset.button;
+      if (+xRegister) {
+        display.innerHTML = formatNumber(+xRegister);
+      } else {
+        display.innerHTML = xRegister;
+      }
     }
   } else {
     if (+this.dataset.button >= 0 || this.dataset.button === ".") {
       yRegister += this.dataset.button;
+      display.innerHTML = formatNumber(+yRegister);
     }
+  }
+
+  if (operators.includes(this.dataset.button)) {
+    operator = this.dataset.button;
   }
 
   if (this.dataset.button === "equals") {
@@ -37,7 +49,29 @@ function handleButtonPress() {
         result = +xRegister * yRegister;
       }
       yRegister = "";
+      xRegister = result;
+      operator = "";
       display.innerHTML = formatNumber(result);
+    }
+  }
+
+  if (this.dataset.button === "reset") {
+    window.location.reload();
+  }
+
+  if (this.dataset.button === "delete") {
+    if (result !== xRegister) {
+      if (!operator) {
+        const xRegisterCopy = xRegister.split("");
+        xRegisterCopy.pop();
+        xRegister = xRegisterCopy.join("");
+        display.innerHTML = formatNumber(+xRegister);
+      } else {
+        const yRegisterCopy = yRegister.split("");
+        yRegisterCopy.pop();
+        yRegister = yRegisterCopy.join("");
+        display.innerHTML = formatNumber(+yRegister);
+      }
     }
   }
 }
