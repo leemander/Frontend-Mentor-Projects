f3434703699f87e1f808dff90d35ac4de1c6754b
crackerFactory <lee.mander@gmail.com>
2022-11-03 11:09:56 +0000

    desktop layout

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -1,3 +1,4 @@
+const form = document.getElementById("form");
 const ipInput = document.getElementById("ip-input");
 const ipEl = document.getElementById("ip");
 const locationEl = document.getElementById("location");
@@ -9,28 +10,36 @@ let locationText;
 let timezone;
 let isp;
 
-const getData = () => {
+map = L.map("map", {
+  attributionControl: false,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+const getData = (ipAddress = "") => {
   fetch(
-    "https://geo.ipify.org/api/v2/country,city?apiKey=at_yjLrZHyDC4kVsYY1eDLpUl8yMmUXI"
+    "https://geo.ipify.org/api/v2/country,city?apiKey=at_yjLrZHyDC4kVsYY1eDLpUl8yMmUXI&ipAddress=" +
+      ipAddress
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       ip = data.ip;
       locationText = `${data.location.city}, ${data.location.region} ${data.location.country}`;
       timezone = data.location.timezone;
       isp = data.isp;
-      map = L.map("map", {
-        attributionControl: false,
-        zoomControl: false,
-        scrollWheelZoom: false,
-      }).setView([data.location.lat, data.location.lng], 18);
+
+      //set the map location
+      map.setView([data.location.lat, data.location.lng], 18);
+
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 19,
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(map);
-      marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
+
+      marker = L.marker([data.location.lat, data.location.lng], {}).addTo(map);
+
+      displayData();
     });
 };
 
@@ -44,4 +53,10 @@ const displayData = () => {
 
 getData();
 
-setTimeout(displayData, 1000);
+//setTimeout(displayData, 1000);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getData(ipInput.value);
+  // setTimeout(displayData, 1000);
+});
