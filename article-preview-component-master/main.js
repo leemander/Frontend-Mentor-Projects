a96cbeb2620c640282dd470e8dde5ee821b50fe0
crackerFactory <lee.mander@gmail.com>
2023-02-27 17:01:41 +0000

    Create desktop layout and share menu functionality

--- article-preview-component-master/main.js
+++ article-preview-component-master/main.js
@@ -4,14 +4,23 @@ const shareMenu = document.getElementById("share-menu");
 const infoElement = document.getElementById("info");
 
 function openShareMenu() {
-  infoElement.classList.add("closed");
+  if (screen.width < 800) infoElement.classList.add("closed");
   shareMenu.classList.add("open");
+  openShareMenuButton.classList.add("active");
 }
 
 function closeShareMenu() {
   infoElement.classList.remove("closed");
   shareMenu.classList.remove("open");
+  openShareMenuButton.classList.remove("active");
 }
 
 openShareMenuButton.addEventListener("click", openShareMenu);
 closeShareMenuButton.addEventListener("click", closeShareMenu);
+
+document.addEventListener("click", (event) => {
+  const { target } = event;
+  if (target !== openShareMenuButton && shareMenu.contains(target) === false) {
+    closeShareMenu();
+  }
+});
