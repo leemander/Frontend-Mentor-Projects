e2fe7940c08fcf988c33e590ee36130a424c8264
crackerFactory <lee.mander@gmail.com>
2023-03-01 11:24:54 +0000

    Refactor JS to improve share menu functionality

--- article-preview-component-master/main.js
+++ article-preview-component-master/main.js
@@ -3,24 +3,34 @@ const closeShareMenuButton = document.getElementById("close-share-menu");
 const shareMenu = document.getElementById("share-menu");
 const infoElement = document.getElementById("info");
 
+let shareMenuIsOpen = false;
+
 function openShareMenu() {
-  if (screen.width < 800) infoElement.classList.add("closed");
+  if (screen.width < 800) infoElement.classList.add("hidden");
   shareMenu.classList.add("open");
   openShareMenuButton.classList.add("active");
+  shareMenuIsOpen = true;
 }
 
 function closeShareMenu() {
-  infoElement.classList.remove("closed");
+  infoElement.classList.remove("hidden");
   shareMenu.classList.remove("open");
   openShareMenuButton.classList.remove("active");
+  shareMenuIsOpen = false;
 }
 
-openShareMenuButton.addEventListener("click", openShareMenu);
 closeShareMenuButton.addEventListener("click", closeShareMenu);
 
 document.addEventListener("click", (event) => {
   const { target } = event;
+
   if (target !== openShareMenuButton && shareMenu.contains(target) === false) {
+    //if the the click target is outside of the share menu or is not the open menu button then close the menu
     closeShareMenu();
+  }
+
+  if (target === openShareMenuButton) {
+    //when the open menu button is clicked either open or close the menu based on its current status
+    shareMenuIsOpen ? closeShareMenu() : openShareMenu();
   }
 });
