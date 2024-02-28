6def5a9b7643090a497faa8a618e4e9e41021a3d
crackerFactory <lee.mander@gmail.com>
2024-02-28 13:53:34 +0000

    Add form validation

--- url-shortening-api-master/script.js
+++ url-shortening-api-master/script.js
@@ -1,6 +1,38 @@
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
+const form = document.getElementById("form");
+const input = document.getElementById("url");
+
+function isValidHttpUrl(str) {
+  //https://www.freecodecamp.org/news/how-to-validate-urls-in-javascript/
+
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$", // fragment locator
+    "i"
+  );
+  return pattern.test(str);
+}
+
+function validateForm() {
+  form.classList.remove("error");
+  if (input.value === "" || !isValidHttpUrl(input.value)) {
+    form.classList.add("error");
+    return false;
+  }
+  return true;
+}
+
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("show");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateForm();
 });
