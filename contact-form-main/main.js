fd98e6c576eb08ca72d15be17aafb386a270f814
crackerFactory <lee.mander@gmail.com>
2024-08-20 15:40:14 +0100

    Add error functionality

--- contact-form-main/main.js
+++ contact-form-main/main.js
@@ -1,4 +1,40 @@
-setTimeout(
-  () => document.getElementById("message").setAttribute("open", ""),
-  500
-);
+//inputs
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const emailInput = document.getElementById("email");
+const generalRadio = document.getElementById("general");
+const supportRadio = document.getElementById("support");
+const messageInput = document.getElementById("message");
+const consentCheckbox = document.getElementById("consent");
+// //errors
+// const firstNameError = document.getElementById("first-name-error");
+// const lastNameError = document.getElementById("last-name-error");
+// const emailError = document.getElementById("email-error");
+// const queryError = document.getElementById("query-error");
+// const messageError = document.getElementById("message-error");
+// const consentError = document.getElementById("consent-error");
+
+const submitButton = document.getElementById("submit");
+
+function validateInput(input) {
+  console.log(input, input.validity.valid);
+  if (!input.validity.valid) {
+    input.setAttribute("data-error", "true");
+  } else {
+    input.removeAttribute("data-error");
+  }
+}
+
+function validateForm() {
+  validateInput(firstNameInput);
+  validateInput(lastNameInput);
+  validateInput(emailInput);
+  validateInput(generalRadio);
+  validateInput(messageInput);
+  validateInput(consentCheckbox);
+}
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  validateForm();
+});
