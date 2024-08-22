0239465af20f7726efef2a49bd1ab790bbd3ca50
crackerFactory <lee.mander@gmail.com>
2024-08-22 14:37:31 +0100

    Add toast message functionality

--- contact-form-main/main.js
+++ contact-form-main/main.js
@@ -6,32 +6,33 @@ const generalRadio = document.getElementById("general");
 const supportRadio = document.getElementById("support");
 const messageInput = document.getElementById("message");
 const consentCheckbox = document.getElementById("consent");
-// //errors
-// const firstNameError = document.getElementById("first-name-error");
-// const lastNameError = document.getElementById("last-name-error");
-// const emailError = document.getElementById("email-error");
-// const queryError = document.getElementById("query-error");
-// const messageError = document.getElementById("message-error");
-// const consentError = document.getElementById("consent-error");
-
 const submitButton = document.getElementById("submit");
 
+const form = document.getElementById("form");
+const toast = document.getElementById("toast");
+
 function validateInput(input) {
-  console.log(input, input.validity.valid);
   if (!input.validity.valid) {
     input.setAttribute("data-error", "true");
+    return false;
   } else {
     input.removeAttribute("data-error");
+    return true;
   }
 }
 
 function validateForm() {
-  validateInput(firstNameInput);
-  validateInput(lastNameInput);
-  validateInput(emailInput);
-  validateInput(generalRadio);
-  validateInput(messageInput);
-  validateInput(consentCheckbox);
+  let valid;
+  valid = validateInput(firstNameInput);
+  valid = validateInput(lastNameInput);
+  valid = validateInput(emailInput);
+  valid = validateInput(generalRadio);
+  valid = validateInput(messageInput);
+  valid = validateInput(consentCheckbox);
+  if (valid) {
+    toast.setAttribute("open", "");
+    form.reset();
+  }
 }
 
 submitButton.addEventListener("click", (e) => {
